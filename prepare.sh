d7d6767fb99defbae5a73ce5411d304ca559d552
Derek Higgins <derekh@redhat.com>
2023-06-20 12:41:15 +0100

    Add iptables

--- prepare.sh
+++ prepare.sh
@@ -2,5 +2,5 @@
 
 
 dnf update -y
-dnf install -y sudo tftp tcpdump net-tools mariadb psmisc nmap qemu-img iproute git procps-ng strace iputils python3 wget e2fsprogs nmstate jq
+dnf install -y sudo tftp tcpdump net-tools mariadb psmisc nmap qemu-img iproute git procps-ng strace iputils python3 wget e2fsprogs nmstate jq iptables
 dnf clean all
